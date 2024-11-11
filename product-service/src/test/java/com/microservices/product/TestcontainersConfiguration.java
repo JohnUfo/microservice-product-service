a9342443c9920d47da4002e48bcfa8b71f5ab11a
JohnUfo <muydinovumidjon17@gmail.com>
2024-11-11 16:53:21 +0500

    first commit

---
+++ product-service/src/test/java/com/microservices/product/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.microservices.product;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
