be4579d8c3f81eb837ec1c17e937db5194f339d6
JohnUfo <muydinovumidjon17@gmail.com>
2024-11-12 15:51:24 +0500

    some unpushed files

--- product-service/src/main/java/com/microservices/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/product/ProductServiceApplication.java
@@ -2,14 +2,12 @@ package com.microservices.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+    //test, mongodb
 }
