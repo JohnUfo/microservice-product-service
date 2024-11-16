9a08e28cbb25fce52130e3505ed86078efd67cfb
JohnUfo <muydinovumidjon17@gmail.com>
2024-11-16 16:31:54 +0500

    still not working

--- product-service/src/test/java/com/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/microservices/product/ProductServiceApplicationTests.java
@@ -6,15 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@ServiceConnection
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
 
 	@LocalServerPort
@@ -27,18 +23,19 @@ class ProductServiceApplicationTests {
 	}
 
 	static {
+		// Start MongoDB container before tests
 		mongoDBContainer.start();
 	}
 
 	@Test
 	void contextLoads() {
 		String requestBody = """
-				{
-				    "name" : "Iphone 16 pro",
-				    "description" : "new",
-				    "price" : 1000
-				}
-				""";
+                {
+                    "name" : "iphone_16_pro",
+                    "description" : "new",
+                    "price" : 1000
+                }
+                """;
 		RestAssured.given()
 				.contentType("application/json")
 				.body(requestBody)
@@ -46,9 +43,8 @@ class ProductServiceApplicationTests {
 				.then()
 				.statusCode(201)
 				.body("id", Matchers.notNullValue())
-				.body("name", Matchers.equalTo("Iphone 16 pro"))
+				.body("name", Matchers.equalTo("iphone_16_pro"))
 				.body("description", Matchers.equalTo("new"))
 				.body("price", Matchers.equalTo(1000));
 	}
-
 }
